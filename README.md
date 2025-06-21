cfb5f543a4a27f178112ba80c399d1887cd6b154
JoseFPortoles <69344137+JoseFPortoles@users.noreply.github.com>
2025-06-21 21:58:59 +0200

    Add example to project description and update conda env for CPU

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# U-Net
-Classic U-Net, pytorch version
+# Just a U-Net
+
+This repository contains an implementation of a classic U-NET architecture, which is widely used for image segmentation tasks. The U-NET model is known for its ability to produce high-quality segmentation maps even with a limited amount of training data.
+
+## Reference
+For more details on the U-NET architecture, please refer to the original paper: 
+Ronneberger, O., Fischer, P., & Brox, T. (2015). U-Net: Convolutional Networks for Biomedical Image Segmentation [<a href="https://doi.org/10.48550/arXiv.1505.04597">arXiv:1505.04597</a>]
+
+## Example
+After training on the HAM10000 skin injury segmentation dataset, which includes expert-drawn segmentation mask annotations, the U-Net demonstrates some capability to predict skin injury segmentations in dermatoscopic images.  
+
+<p align="center">
+  <img src="assets/composite_image_ISIC_0000031.png" alt="Original image (left), prediction (center), superposition (right)">
+  <br>
+  <em> Melanoma and U-Net prediction mask. Original image (left), prediction (center), superposition (right). Source for the original image <a href="https://gallery.isic-archive.com/">ISIC Archive (Unique ID: ISIC_0000031)</a>. Original image published under Creative Commons CC-0 copyright license.
+  </em>
+</p> 
+
+## Further details
+It includes Dataset classes for loading data from: 
+* [VOC2012](http://host.robots.ox.ac.uk/pascal/VOC/): A classic 1000 everyday object categories semantic segmentation dataset. 
+* [HAM10000](https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/DBW86T): A skin injury segmentation dataset. 
--- scripts/test.py
+++ scripts/test.py
@@ -1,13 +1,16 @@
 import PIL.Image
+import PIL
+import matplotlib.cm as cm
 import torch
 import torchvision
+from torchvision import transforms as T
+import numpy as np
 import argparse
-import os
+from pathlib import Path
 from models.ext_unet import UNet
 from transforms.transforms import test_transform
-import PIL
-import numpy as np
-from pathlib import Path
+
+ALPHA = 0.7
 
 parser = argparse.ArgumentParser(prog='test.py', description='Test U-Net on test dataset images')
 parser.add_argument('--input_size', type=int, default=224, help="Input image size.")
@@ -33,13 +36,40 @@ if args.weights_path:
     else:
         raise ValueError(f"Specified weights path {args.weights_path} does not exist, model was xavier initialised")
 
-input_img = np.array(PIL.Image.open(args.test_img_path).convert('RGB'))
+input_img = PIL.Image.open(args.test_img_path).convert('RGB')
+original_size = input_img.size
 
-pred = unet(test_transform(args.input_size)(image=input_img)['image'].unsqueeze(0)).squeeze(0)
+input_array = np.array(input_img)
+pred = unet(test_transform(args.input_size)(image=input_array)['image'].unsqueeze(0)).squeeze(0)
 
-pred_mask = ((pred - pred.min())/(pred.max() - pred.min())*255.).to(torch.int8).squeeze(0).numpy()
+pred_mask = ((pred - pred.min())/(pred.max() - pred.min())*255.).to(torch.uint8).squeeze(0).numpy()
+resized_input_img = input_img.resize(pred_mask.shape[::-1])
 
 print(f"type: {type(pred)}, shape: {pred.shape}")
 if Path(args.output_path).is_dir() is False:
     Path(args.output_path).mkdir(parents=True, exist_ok=True)
-PIL.Image.fromarray(pred_mask, mode='L').save(Path(args.output_path)/f"pred_mask_{Path(args.test_img_path).stem}.png")
+
+
+# Convert the grayscale image to a color heatmap
+heatmap = cm.jet(pred_mask)
+heatmap_rgb = np.delete(heatmap, 3, 2)*255.
+
+
+# Save the color heatmap image
+heatmap_img = PIL.Image.fromarray(heatmap_rgb.astype(np.uint8))
+#PIL.Image.fromarray((heatmap_rgb * 255).astype(np.uint8)).save(Path(args.output_path)/f"pred_mask_{Path(args.test_img_path).stem}.png")
+
+
+# Resize all three images to the original size
+heatmap_img = heatmap_img.resize(original_size)
+resized_input_img = resized_input_img.resize(original_size)
+blended_image = PIL.Image.blend(heatmap_img, resized_input_img, ALPHA)
+
+# Save the blended image
+blended_image.save(Path(args.output_path)/f"blended_image_{Path(args.test_img_path).stem}.png")
+
+# Save resized input image
+# resized_input_img.save(Path(args.output_path)/f"resized_input_{Path(args.test_img_path).stem}.png")
+
+composite_image = PIL.Image.fromarray(np.hstack((np.array(resized_input_img), np.array(heatmap_img), np.array(blended_image)))) 
+composite_image.save(Path(args.output_path)/f"composite_image_{Path(args.test_img_path).stem}.png")
